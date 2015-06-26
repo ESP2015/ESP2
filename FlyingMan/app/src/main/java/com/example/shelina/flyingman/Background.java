1642f799d72555860e3297d09c4519e3a91341ed
wsameen2014 <wsameen2014@fau.edu>
2015-06-25 20:19:51 -0400

    Progress Report
    Pierre, Wasee, and Ryan

---
+++ FlyingMan/app/src/main/java/com/example/shelina/flyingman/Background.java
@@ -0,0 +1,44 @@
+package com.example.shelina.flyingman;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Shelina on 6/25/2015.
+ */
+public class Background {
+    import android.graphics.Bitmap;
+    import android.graphics.Canvas;
+
+    public class Background {
+
+        private Bitmap image;
+        private int x, y, dx;
+
+        public Background(Bitmap res)
+        {
+            image = res;
+        }
+        public void update()
+        {
+            x+=dx;
+            if(x<-GamePanel.WIDTH){
+                x=0;
+            }
+        }
+        public void draw(Canvas canvas)
+        {
+            canvas.drawBitmap(image, x, y,null);
+            if(x<0)
+            {
+                canvas.drawBitmap(image, x+GamePanel.WIDTH, y, null);
+            }
+        }
+        public void setVector(int dx)
+        {
+            this.dx = dx;
+        }
+    }
+
+
+
+}
